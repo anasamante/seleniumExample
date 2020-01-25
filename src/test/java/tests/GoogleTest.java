d269a6cf80b29ba5304aa5d589adf9de0bed460b
Ana Amante <ana.amante@overactiveinc.com>
2020-01-25 11:57:56 -0300

    refactor

---
+++ src/test/java/tests/GoogleTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.GoogleSearchPage;
+
+public class GoogleTest extends BaseTest {
+
+    @Test
+    public void searchNoLoginTest(){
+
+        String searchPattern = "CNN";
+
+        GoogleSearchPage googleSearchPage = new GoogleSearchPage(driver);
+        googleSearchPage.enterSearchText(searchPattern);
+        googleSearchPage.clickSearchButton();
+        googleSearchPage.isMatchFound(searchPattern);
+        Assert.assertTrue(googleSearchPage.isSearchFound(searchPattern));
+    }
+
+    @Test
+    public void anotherSearchNoLoginTest(){
+
+        String searchPattern = "ABC";
+
+        GoogleSearchPage googleSearchPage = new GoogleSearchPage(driver);
+        googleSearchPage.enterSearchText(searchPattern);
+        googleSearchPage.clickSearchButton();
+        googleSearchPage.isMatchFound(searchPattern);
+        Assert.assertTrue(googleSearchPage.isSearchFound(searchPattern));
+    }
+
+
+}
